8a2ef2f93b7b314e5be9f74e3ef63840f70bacb6
Modeste Ciira <ciiramodeste23@gmail.com>
2023-11-30 16:13:52 +0300

    making use of another class inside the present class and returning results

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = typeof amount === 'number' ? amount : 0;
+    this._currency = currency instanceof Currency ? currency : new Currency('Unkwon', 'UNK');
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+}
