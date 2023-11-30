10130fbc21d828dfe6a073eaaaf6296533ee9997
Modeste Ciira <ciiramodeste23@gmail.com>
2023-11-30 16:18:16 +0300

    Primitive - Holberton Class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,22 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = typeof size === 'number' ? size : 0;
+    this._location = typeof location === 'string' ? location : '';
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
