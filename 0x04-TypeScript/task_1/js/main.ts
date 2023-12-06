9fc10f66ee2a723039bf1b385f3c359374df69a4
Modeste Ciira <ciiramodeste23@gmail.com>
2023-12-07 00:07:31 +0300

    Printing teachers, Extending teacher class and teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,7 @@
+// task 1
 interface TeacherBase {
+  firstName: string;
+  lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience: number;
   location: string;
@@ -8,6 +11,12 @@ type AdditionalAttributes = {
   [key: string]: any;
 };
 
+// task 2
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// task 1.
 class Teacher implements TeacherBase {
   firstName: string;
   lastName: string;
@@ -27,5 +36,41 @@ class Teacher implements TeacherBase {
         this.additionalAttributes[key] = teacherData[key];
       }
     }
+  }
+}
+
+// task 3.
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstLetter = firstName.charAt(0);
+  return `${firstLetter}. ${lastName}`;
+};
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+interface ClassStudent {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements ClassStudent {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
   }
 }
