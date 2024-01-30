f1d949dc5ba7a4251f4f9c976bb2d90225fc21b0
Modeste74 <ciiramodeste23@gmail.com>
2024-01-29 21:51:17 -1200

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
+  console.log(`Your name is: ${answer}`);
+  rl.close();
+});
+if (!process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    const input = data.toString().trim();
+    console.log('This important software is now closing');
+  });
+}
