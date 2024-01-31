f6dd0afffe79e94706ee0800da6ce208f64cb35f
Modeste74 <ciiramodeste23@gmail.com>
2024-01-31 07:08:45 -1200

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,7 @@
+function calculateNumber (a, b) {
+  const aRounded = Math.round(a);
+  const bRounded = Math.round(b);
+  return aRounded + bRounded;
+}
+
+module.exports = calculateNumber
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber function', () => {
+  it('should round and return sum of two positive params', () => {
+    const result = calculateNumber(2.5, 3.7);
+    assert.strictEqual(result, 7);
+  });
+
+  it('should round and return sum of two negative params', () => {
+    const result = calculateNumber(-2.5, -3.7);
+    assert.strictEqual(result, -6);
+  });
+
+  it('should round and add a positve to a negative param', () => {
+    const result = calculateNumber(2.5, -3.7);
+    assert.strictEqual(result, -1);
+  });
+
+  it('should round and add two whole numbers', () => {
+    const result = calculateNumber(3, 11);
+    assert.strictEqual(result, 14);
+  });
+
+  it('should round and a decimal and whole number', () => {
+    const result = calculateNumber(2.5, 7);
+    assert.strictEqual(result, 10);
+  });
+});
