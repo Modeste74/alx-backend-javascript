fbc7c3a81d07d0cec76b4ed15e3d97ff37595a18
Modeste74 <ciiramodeste23@gmail.com>
2024-02-01 01:15:03 -1200

    implementing hooks in testing

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+
+const { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./5-payment');
+
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleLogSpy.restore();
+  });
+
+  it('should log the correct total in the console for 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('should log the correct total in the console for 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+  });
+});
