815c7c93fb9e6e538bc2fec43b202655f67eeeca
Modeste74 <ciiramodeste23@gmail.com>
2024-02-01 07:18:01 -1200

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`API available on http://localhost:${port}`);
+  console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app; 
