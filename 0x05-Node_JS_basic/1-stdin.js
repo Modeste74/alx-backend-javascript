09241403dc76f1500eb8a1979119914b8bd7ea6a
Modeste74 <ciiramodeste23@gmail.com>
2024-02-01 23:56:53 -1200

    using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,12 @@
 console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.setEncoding('utf8');
+
 process.stdin.on('readable', () => {
   const readInput = process.stdin.read();
   if (readInput) process.stdout.write(`Your name is: ${readInput}`);
 });
+
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
